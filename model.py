fb96299dff42c34e078286a1d873a57134e19d04
Mohan-afk <88244592+Mohan-afk@users.noreply.github.com>
2026-06-21 01:21:05 +0530

    Step 32: apply_ffn_first_linear_and_relu

--- model.py
+++ model.py
@@ -268,8 +268,10 @@ def assemble_multi_head_attention_forward(query, key, value, w_q, w_k, w_v, w_o,
 
     return merge_heads_and_project_output(context_h,w_o,b_o=None)
 
-# Step 32 - apply_ffn_first_linear_and_relu (not yet solved)
-# TODO: implement
+# Step 32 - apply_ffn_first_linear_and_relu
+def apply_ffn_first_linear_and_relu(x, w1, b1):
+    # TODO: project x by w1, add b1, then apply a ReLU activation.
+    return torch.relu(torch.matmul(x,w1)+b1)
 
 # Step 33 - apply_ffn_second_linear (not yet solved)
 # TODO: implement
